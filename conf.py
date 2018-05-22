d76b5886cc081cdeb21ef7f20aa4b9b0fe8a50be
DeathOfRats <deathofrats@protonmail.ch>
2018-05-22 02:20:34 +0200

    try

--- conf.py
+++ conf.py
@@ -147,7 +147,7 @@ NAVIGATION_ALT_LINKS = {
 }
 
 # Name of the theme to use.
-THEME = "cadair"
+THEME = "bootblog4"
 
 # Primary color of your theme. This will be used to customize your theme.
 # Must be a HEX value.
